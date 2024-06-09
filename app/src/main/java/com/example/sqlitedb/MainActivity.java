aa12fd89e28221832a5998265ab022e7ab7a6d9a
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-06-09 10:35:08 +0600

    add layout for login feature

--- app/src/main/java/com/example/sqlitedb/MainActivity.java
+++ app/src/main/java/com/example/sqlitedb/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.sqlitedb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import register.Register_Field;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginBtn, registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.register_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ));
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register_Field.class));
+            }
+        });
     }
 }
