3ea73c67d2dc9c6082793a3329cf5e71b404ead9
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-06-11 09:33:36 +0600

    add user details features

--- app/src/main/java/com/example/sqlitedb/UserDetails.java
+++ app/src/main/java/com/example/sqlitedb/UserDetails.java
@@ -3,8 +3,13 @@ package com.example.sqlitedb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class UserDetails extends AppCompatActivity {
+
+    TextView name, email, phone, password;
+    Button updateBtn, deleteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
